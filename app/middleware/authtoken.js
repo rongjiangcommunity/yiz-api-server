80e4eab6bbfe00aa6fcf851312fb0ed3638c52e8
echopi <eagleking0213@gmail.com>
2019-03-12 01:23:57 +0800

    middleware authtoken

---
+++ app/middleware/authtoken.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = () => {
+  return async (ctx, next) => {
+    const token = ctx.headers['X-Auth-Token'] || ctx.headers['x-auth-token'];
+    ctx.logger.info('x-auth-token', token);
+    if (token !== ctx.app.config['authtoken']) {
+      ctx.status = 403;
+      ctx.body = {
+        success: false,
+        data: 'Forbidden',
+        status: 403,
+      };
+      return;
+    }
+    await next();
+  };
+};
